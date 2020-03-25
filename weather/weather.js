2e7de67d81b396b3860444671a5c74c5c89ababa
unknown <motea927@yahoo.com.tw>
2020-03-25 23:00:45 +0800

    make it better

--- weather/weather.js
+++ weather/weather.js
@@ -4,8 +4,12 @@ if (!key) key = require('../weather-key')
 const getWeather = async (lat, lng) => {
   try {
     const response = await axios.get(`https://api.darksky.net/forecast/${key}/${lat},${lng}?exclude=daily&lang=zh-tw&units=si`)
+    const time = [response.data.hourly.data[0].time, response.data.hourly.data[1].time]
+    time.forEach((UNIX_Timestamp) => {
+      const date = new Date(UNIX_Timestamp * 1000)
+      console.log(date.toTimeString())
+    })
     console.log(response.data.hourly.data[0])
-    console.log(response.data.hourly.data.length)
   } catch (err) {
     console.error(err)
   }
