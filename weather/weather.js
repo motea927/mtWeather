f54a32315d0ab4986ace54dbb185994c22579143
unknown <motea927@yahoo.com.tw>
2020-03-25 22:34:47 +0800

    make it better

--- weather/weather.js
+++ weather/weather.js
@@ -4,7 +4,8 @@ if (!key) key = require('../weather-key')
 const getWeather = async (lat, lng) => {
   try {
     const response = await axios.get(`https://api.darksky.net/forecast/${key}/${lat},${lng}`)
-    console.log(response.data)
+    console.log(response.data[0])
+    console.log(`length: ${response.data.length}`)
   } catch (err) {
     console.error(err)
   }
