c4930848e18c7cddce2a8a80ecb8f803842dbe0e
unknown <motea927@yahoo.com.tw>
2020-03-26 00:32:27 +0800

    make it better

--- weather/weather.js
+++ weather/weather.js
@@ -7,7 +7,7 @@ const getWeather = async (lat, lng) => {
     const time = [response.data.hourly.data[0].time, response.data.hourly.data[1].time]
     time.forEach((UNIX_Timestamp) => {
       const date = new Date((UNIX_Timestamp + 28800)* 1000)
-      console.log(date)
+      console.log(date.toLocaleString('zh-TW'))
     })
     console.log(response.data.hourly.data[0])
   } catch (err) {
