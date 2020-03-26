c1f05bcbabad49b8666f48a17a20179ba72e2e7a
unknown <motea927@yahoo.com.tw>
2020-03-26 13:41:35 +0800

    make it better

--- lineClient/lineClient.js
+++ lineClient/lineClient.js
@@ -12,59 +12,7 @@ const lineClient = {
       addressEndIndex = address.lastIndexOf(address.match(re).pop())
       address = address.slice(0, addressEndIndex + 1)
     }
-    
     const weatherResult = await weather.getWeather(lat, lng)
-    const data = [
-      {
-        "type": "box",
-        "layout": "baseline",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "text",
-            "text": "時間",
-            "size": "md",
-            "weight": "bold"
-          },
-          {
-            "type": "text",
-            "text": "溫度",
-            "size": "md",
-            "weight": "bold"
-          },
-          {
-            "type": "text",
-            "text": "降雨機率",
-            "size": "md",
-            "weight": "bold"
-          }
-        ]
-      }
-    ]
-    weatherResult.hourlyDataArr.forEach(element => {
-      data.push({
-        "type": "box",
-        "layout": "baseline",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "text",
-            "text": `${element.time}`,
-            "size": "sm"
-          },
-          {
-            "type": "text",
-            "text": `${element.temperature} °C`,
-            "wrap": true,
-            "size": "sm"
-          },
-          {
-            "type": "text",
-            "text": `${Math.round(element.precipProbability * 100)}%`
-          }
-        ]
-      })
-    });
     const replyMessage = [
       {
         "type": "flex",
@@ -73,15 +21,33 @@ const lineClient = {
         "contents": {
           "type": "bubble",
           "hero": {
-            "type": "image",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-            "action": {
-              "type": "uri",
-              "uri": "http://linecorp.com/"
-            }
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "image",
+                "url": "https://mt-weather.herokuapp.com/static/rain.png",
+                "align": "end",
+                "offsetEnd": "15px",
+                "offsetTop": "15px"
+              },
+              {
+                "type": "text",
+                "text": "28.16°C",
+                "color": "#B8B8B8",
+                "size": "xxl",
+                "offsetStart": "25px",
+                "margin": "md"
+              },
+              {
+                "type": "text",
+                "text": "潮濕",
+                "color": "#d2d2d2",
+                "size": "lg",
+                "offsetStart": "25px"
+              }
+            ],
+            "height": "170px"
           },
           "body": {
             "type": "box",
@@ -89,14 +55,14 @@ const lineClient = {
             "contents": [
               {
                 "type": "text",
-                "text": `${address}`,
+                "text": "台北市中正區",
                 "weight": "bold",
                 "size": "lg",
                 "margin": "md"
               },
               {
                 "type": "text",
-                "text": `${weatherResult.summary}`,
+                "text": "小雨持續至明晚。",
                 "size": "sm",
                 "color": "#9c9c9c",
                 "margin": "md"
@@ -106,7 +72,143 @@ const lineClient = {
                 "layout": "vertical",
                 "margin": "lg",
                 "spacing": "sm",
-                "contents": data
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "時間",
+                        "size": "md",
+                        "weight": "bold"
+                      },
+                      {
+                        "type": "text",
+                        "text": "溫度",
+                        "size": "md",
+                        "weight": "bold"
+                      },
+                      {
+                        "type": "text",
+                        "text": "降雨機率",
+                        "size": "md",
+                        "weight": "bold"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "17:00",
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "28.16°C",
+                        "wrap": true,
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "50%"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "18:00",
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "27.21°C",
+                        "wrap": true,
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "50%"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "19:00",
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "27.64°C",
+                        "wrap": true,
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "50%"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "20:00",
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "28°C",
+                        "wrap": true,
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "50%"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "21:00",
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "35°C",
+                        "wrap": true,
+                        "size": "sm"
+                      },
+                      {
+                        "type": "text",
+                        "text": "50%"
+                      }
+                    ]
+                  }
+                ]
               }
             ]
           }
