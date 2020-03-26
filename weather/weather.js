140e354c0f7ffb356d453cdfc281b4d2ff873ea1
unknown <motea927@yahoo.com.tw>
2020-03-26 16:56:45 +0800

    make it better

--- weather/weather.js
+++ weather/weather.js
@@ -19,7 +19,7 @@ const getWeather = async (lat, lng) => {
     })
     daily.data.forEach(dailyData => {
       const date = new Date((dailyData.time + 28800) * 1000)
-      dailyData.time = `${date.getFullYear()}/${addZeroString(date.getMonth() + 1)}/${addZeroString(date.getDate())}`
+      dailyData.time = `${addZeroString(date.getMonth() + 1)}/${addZeroString(date.getDate())}`
       dailyData.temperature = `${dailyData.temperatureMin} ~ ${dailyData.temperatureMax}`
     })
     return {
