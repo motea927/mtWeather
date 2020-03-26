8c68e76f503521c8a33824ef7981549820d9e7f7
unknown <motea927@yahoo.com.tw>
2020-03-26 15:25:40 +0800

    make it better

--- lineClient/lineClient.js
+++ lineClient/lineClient.js
@@ -68,346 +68,75 @@ const lineClient = {
         }
       )
     })
-    /*
+    const bubbleMessage = {
+      "type": "bubble",
+      "hero": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": `https://mt-weather.herokuapp.com/static/${weatherResult.currently.icon}.png`,
+            "align": "end",
+            "offsetEnd": "15px",
+            "offsetTop": "15px",
+            "size": "sm"
+          },
+          {
+            "type": "text",
+            "text": `${weatherResult.currently.temperature} °C`,
+            "color": "#B8B8B8",
+            "size": "xxl",
+            "offsetStart": "25px",
+            "margin": "md"
+          },
+          {
+            "type": "text",
+            "text": `${weatherResult.currently.summary}`,
+            "color": "#d2d2d2",
+            "size": "lg",
+            "offsetStart": "25px"
+          }
+        ],
+        "height": "150px"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": address,
+            "weight": "bold",
+            "size": "lg",
+            "margin": "md"
+          },
+          {
+            "type": "text",
+            "text": `${weatherResult.hourly.summary}`,
+            "size": "sm",
+            "color": "#9c9c9c",
+            "margin": "md"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "lg",
+            "spacing": "sm",
+            "contents": weatherData
+          }
+        ]
+      }
+    }
     const replyMessage = [ 
       {
         "type": "flex",
         "altText": "未來六小時天氣",
         // contents
-        "contents": {
-          "type": "bubble",
-          "hero": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "image",
-                "url": `https://mt-weather.herokuapp.com/static/${weatherResult.currently.icon}.png`,
-                "align": "end",
-                "offsetEnd": "15px",
-                "offsetTop": "15px",
-                "size": "sm"
-              },
-              {
-                "type": "text",
-                "text": `${weatherResult.currently.temperature} °C`,
-                "color": "#B8B8B8",
-                "size": "xxl",
-                "offsetStart": "25px",
-                "margin": "md"
-              },
-              {
-                "type": "text",
-                "text": `${weatherResult.currently.summary}`,
-                "color": "#d2d2d2",
-                "size": "lg",
-                "offsetStart": "25px"
-              }
-            ],
-            "height": "150px"
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": address,
-                "weight": "bold",
-                "size": "lg",
-                "margin": "md"
-              },
-              {
-                "type": "text",
-                "text": `${weatherResult.hourly.summary}`,
-                "size": "sm",
-                "color": "#9c9c9c",
-                "margin": "md"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "margin": "lg",
-                "spacing": "sm",
-                "contents": weatherData
-              }
-            ]
-          }
-        }
+        "contents": bubbleMessage
         // contents end
       }
     ]
-    */
-    const replyMessage = 
-      {
-        "type": "flex",
-        "altText": "未來六小時天氣",
-        "contents": {
-          "type": "carousel",
-          "contents": [
-            {
-              "type": "bubble",
-              "hero": {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip10.jpg",
-                "size": "full",
-                "aspectMode": "cover",
-                "aspectRatio": "320:213"
-              },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Brown Cafe",
-                    "weight": "bold",
-                    "size": "sm",
-                    "wrap": true
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                      },
-                      {
-                        "type": "text",
-                        "text": "4.0",
-                        "size": "xs",
-                        "color": "#8c8c8c",
-                        "margin": "md",
-                        "flex": 0
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "spacing": "sm",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "東京旅行",
-                            "wrap": true,
-                            "color": "#8c8c8c",
-                            "size": "xs",
-                            "flex": 5
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ],
-                "spacing": "sm",
-                "paddingAll": "13px"
-              },
-              "size": "giga"
-            },
-            {
-              "type": "bubble",
-              "hero": {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip11.jpg",
-                "size": "full",
-                "aspectMode": "cover",
-                "aspectRatio": "320:213"
-              },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Brow&Cony's Restaurant",
-                    "weight": "bold",
-                    "size": "sm",
-                    "wrap": true
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                      },
-                      {
-                        "type": "text",
-                        "text": "4.0",
-                        "size": "sm",
-                        "color": "#8c8c8c",
-                        "margin": "md",
-                        "flex": 0
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "spacing": "sm",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "東京旅行",
-                            "wrap": true,
-                            "color": "#8c8c8c",
-                            "size": "xs",
-                            "flex": 5
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ],
-                "spacing": "sm",
-                "paddingAll": "13px"
-              },
-              "size": "giga"
-            },
-            {
-              "type": "bubble",
-              "size": "giga",
-              "hero": {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip12.jpg",
-                "size": "full",
-                "aspectMode": "cover",
-                "aspectRatio": "320:213"
-              },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Tata",
-                    "weight": "bold",
-                    "size": "sm"
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                      },
-                      {
-                        "type": "icon",
-                        "size": "xs",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                      },
-                      {
-                        "type": "text",
-                        "text": "4.0",
-                        "size": "sm",
-                        "color": "#8c8c8c",
-                        "margin": "md",
-                        "flex": 0
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "spacing": "sm",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "東京旅行",
-                            "wrap": true,
-                            "color": "#8c8c8c",
-                            "size": "xs",
-                            "flex": 5
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ],
-                "spacing": "sm",
-                "paddingAll": "13px"
-              }
-            }
-          ]
-        }
-
-      }
-      
-      
     client.replyMessage(replyToken, replyMessage)
   },
   sendLocationBtn (replyToken) {
