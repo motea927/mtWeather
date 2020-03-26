2c0d4b2853678d5c5f66394c9266ade46e372716
unknown <motea927@yahoo.com.tw>
2020-03-26 17:26:42 +0800

    make it better

--- weather/weather.js
+++ weather/weather.js
@@ -6,6 +6,7 @@ function addZeroString (num) {
   if (num >= 10) return num
   return `0${num}`
 }
+const weekDay = ['日', '一', '二', '三', '四', '五', '六']
 const getWeather = async (lat, lng) => {
   try {
     const response = await axios.get(`https://api.darksky.net/forecast/${key}/${lat},${lng}?exclude=minutely,flags&lang=zh-tw&units=si`)
@@ -19,7 +20,7 @@ const getWeather = async (lat, lng) => {
     })
     daily.data.forEach(dailyData => {
       const date = new Date((dailyData.time + 28800) * 1000)
-      dailyData.time = `${addZeroString(date.getMonth() + 1)}/${addZeroString(date.getDate())}`
+      dailyData.time = `${addZeroString(date.getMonth() + 1)}/${addZeroString(date.getDate())} (${weekDay[date.getDate()]})`
       dailyData.temperature = `${Math.round(dailyData.temperatureMin)}~${Math.round(dailyData.temperatureMax)}`
     })
     return {
