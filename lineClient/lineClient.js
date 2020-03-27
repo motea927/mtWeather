75d29ab5da3453293f0ce2c3e0c34770e198c897
unknown <motea927@yahoo.com.tw>
2020-03-27 15:51:56 +0800

    make it better

--- lineClient/lineClient.js
+++ lineClient/lineClient.js
@@ -59,17 +59,7 @@ const lineClient = {
                         "type": "postback",
                         "label": "基隆市",
                         "data": "基隆市"
-                    },
-                    {
-                      "type": "postback",
-                      "label": "基隆市",
-                      "data": "基隆市"
-                  },
-                  {
-                    "type": "postback",
-                    "label": "基隆市",
-                    "data": "基隆市"
-                }
+                    }
                 ]
               }
           ],
