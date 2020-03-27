8803ea6c0d71a07de43454c2cb6c29aa4b152c42
unknown <motea927@yahoo.com.tw>
2020-03-27 16:11:08 +0800

    make it better

--- lineClient/lineClient.js
+++ lineClient/lineClient.js
@@ -11,14 +11,14 @@ const lineClient = {
       "type": "bubble",
       "hero": {
         "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
         "size": "full",
         "aspectRatio": "20:13",
         "aspectMode": "cover",
         "action": {
           "type": "uri",
           "uri": "http://linecorp.com/"
-        }
+        },
+        "url": "https://www.travel.taipei/image/64607/1024x768"
       },
       "body": {
         "type": "box",
@@ -26,103 +26,20 @@ const lineClient = {
         "contents": [
           {
             "type": "text",
-            "text": "Brown Cafe",
+            "text": "北部地區",
             "weight": "bold",
             "size": "xl"
           },
           {
-            "type": "box",
-            "layout": "baseline",
-            "margin": "md",
-            "contents": [
-              {
-                "type": "icon",
-                "size": "sm",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-              },
-              {
-                "type": "icon",
-                "size": "sm",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-              },
-              {
-                "type": "icon",
-                "size": "sm",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-              },
-              {
-                "type": "icon",
-                "size": "sm",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-              },
-              {
-                "type": "icon",
-                "size": "sm",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-              },
-              {
-                "type": "text",
-                "text": "4.0",
-                "size": "sm",
-                "color": "#999999",
-                "margin": "md",
-                "flex": 0
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "vertical",
-            "margin": "lg",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "box",
-                "layout": "baseline",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Place",
-                    "color": "#aaaaaa",
-                    "size": "sm",
-                    "flex": 1
-                  },
-                  {
-                    "type": "text",
-                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                    "wrap": true,
-                    "color": "#666666",
-                    "size": "sm",
-                    "flex": 5
-                  }
-                ]
-              },
-              {
-                "type": "box",
-                "layout": "baseline",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Time",
-                    "color": "#aaaaaa",
-                    "size": "sm",
-                    "flex": 1
-                  },
-                  {
-                    "type": "text",
-                    "text": "10:00 - 23:00",
-                    "wrap": true,
-                    "color": "#666666",
-                    "size": "sm",
-                    "flex": 5
-                  }
-                ]
-              }
-            ]
+            "type": "text",
+            "text": "北北基彰中桃苗",
+            "size": "sm",
+            "offsetStart": "1px",
+            "color": "#707070"
           }
-        ]
+        ],
+        "borderColor": "#33333333",
+        "borderWidth": "1px"
       },
       "footer": {
         "type": "box",
@@ -134,9 +51,10 @@ const lineClient = {
             "style": "link",
             "height": "sm",
             "action": {
-              "type": "uri",
-              "label": "CALL",
-              "uri": "https://linecorp.com"
+              "type": "postback",
+              "label": "hello",
+              "data": "hello",
+              "displayText": "hello"
             }
           },
           {
@@ -144,14 +62,33 @@ const lineClient = {
             "style": "link",
             "height": "sm",
             "action": {
-              "type": "uri",
-              "label": "WEBSITE",
-              "uri": "https://linecorp.com"
+              "type": "postback",
+              "label": "hello",
+              "data": "hello",
+              "displayText": "hello"
             }
           },
           {
-            "type": "spacer",
-            "size": "sm"
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "postback",
+              "label": "hello",
+              "data": "hello",
+              "displayText": "hello"
+            }
+          },
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "postback",
+              "label": "hello",
+              "data": "hello",
+              "displayText": "hello"
+            }
           }
         ],
         "flex": 0
