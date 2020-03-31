e4abca742d7108a89647c676ada26885268c9079
unknown <motea927@yahoo.com.tw>
2020-03-31 15:15:10 +0800

    make it better

--- lineClient/lineClient.js
+++ lineClient/lineClient.js
@@ -205,8 +205,7 @@ const lineClient = {
           "layout": "horizontal",
           "contents": locationBtnMessage
         })
-        if ((index + 1) !== locationArr.length) {
-          console.log(index, locationArr.length)
+        if (index !== locationArr.length - 2) {
           locationBtnBox.push(
             {
             "type": "separator"}
